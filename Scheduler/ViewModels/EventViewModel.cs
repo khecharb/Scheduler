273113eafc92cf25614511f46c35e2b5982f5549
nicisbetter <nick.lihway.hill@gmail.com>
2016-02-22 12:47:05 -0600

    Added Some Events Controllers, Views

---
+++ Scheduler/ViewModels/EventViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Scheduler.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Scheduler.ViewModels {
+    public class EventViewModel {
+        public List<Event> Events { get; set; }
+        public List<Role> Roles { get; set; }
+
+        public EventViewModel() {
+            Events = Event.getAll();
+            Roles = Role.getAll();
+
+        }
+
+
+    }
+}
